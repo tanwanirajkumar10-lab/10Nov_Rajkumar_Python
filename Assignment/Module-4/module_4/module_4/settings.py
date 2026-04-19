a44a8f02b4d370c76e1ec10da1549b211815af0e
tanwanirajkumar10-lab <tanwanirajkumar10@gmail.com>
2026-04-19 12:32:18 +0530

    Update settings.py

--- Assignment/Module-4/module_4/module_4/settings.py
+++ Assignment/Module-4/module_4/module_4/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0xj7o0jccy@ya2dlu+$c8xqy9c$pqcft@o=2^zb6t+ir@_x(&a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
