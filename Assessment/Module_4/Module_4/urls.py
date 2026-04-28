12f353d0f57e67979649b3dc8b57a8486ae32404
unknown <tanwanirajkumar10@gmail.com>
2026-04-29 03:07:49 +0530

    feat: add premium 404 and 500 error pages with preview routes

--- Assessment/Module_4/Module_4/urls.py
+++ Assessment/Module_4/Module_4/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('writesphere.urls')),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
+    
+    # Error Page Previews (Only for development)
+    path('404/', TemplateView.as_view(template_name='404.html')),
+    path('500/', TemplateView.as_view(template_name='500.html')),
 ]
 
 if settings.DEBUG:
